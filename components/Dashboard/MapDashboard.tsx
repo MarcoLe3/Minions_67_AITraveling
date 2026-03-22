02bf7686eb63b1cadba5bb36f636993d3fa76db3
Marco Lee <marcole32k3@gmail.com>
2026-03-21 22:35:38 -0700

    starting dashboard for map

--- components/Dashboard/MapDashboard.tsx
+++ components/Dashboard/MapDashboard.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use client'
 
 import Image from "next/image"
@@ -69,5 +70,27 @@ export function MainDashboard(props: MapDashboardProps){
             {openSettingDashboard && <SettingDashboard/>}
         </div>
 
+=======
+import Image from "next/image"
+
+export function MapDashboard(props:object){
+    return (
+        <section
+            className="h-screen w-[300px]"
+        >
+            <Image
+                src={`${props.image}`}
+                alt="Place Image"
+                width={300}
+                height={300}
+            />
+            <h2>
+                {props.heading}
+            </h2>
+            <p>
+                {props.rating}
+            </p>
+        </section>
+>>>>>>> e1a44b0 (starting dashboard for map)
     )
 }
