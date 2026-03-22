e0cfa4a8182c0d62efe0648d53de17b659b9ee6d
Marco Lee <marcole32k3@gmail.com>
2026-03-21 22:35:38 -0700

    starting dashboard for map

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="">
-      <section
-        className="h-[300px] w-[300px]"
-      >
-      </section>
+    <div className=" bg-foreground">
+
     </div>
   );
 }
