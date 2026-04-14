c02922d9e87796189854cd2f9f80c2b0f7f18f2f
bennguyen0 <bnguyen684@student.fuhsd.org>
2026-04-14 00:22:52 -0700

    small tweak to radiogroup - fix spelling, form label to relate to travel

--- my-next-app/components/Radio/RadioGroup.tsx
+++ my-next-app/components/Radio/RadioGroup.tsx
@@ -7,13 +7,13 @@ import FormLabel from '@mui/material/FormLabel';
 export default function RadioButtonsGroup() {
   return (
     <FormControl>
-      <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+      <FormLabel id="demo-radio-buttons-group-label">Purpose of Trip</FormLabel>
       <RadioGroup
         aria-labelledby="demo-radio-buttons-group-label"
         defaultValue="female"
         name="radio-buttons-group"
       >
-        <FormControlLabel value="siteseening" control={<Radio />} label="Site Seening" />
+        <FormControlLabel value="sightseeing" control={<Radio />} label="Sightseeing" />
         <FormControlLabel value="shopping" control={<Radio />} label="Shopping" />
         <FormControlLabel value="other" control={<Radio />} label="Other" />
       </RadioGroup>
