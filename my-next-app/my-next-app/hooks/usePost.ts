5618046f1171e6cc94a8cce1633409048c3c67ce
Marco Lee <marcole32k3@gmail.com>
2026-04-15 22:43:56 -0700

    testing what git is

---
+++ my-next-app/my-next-app/hooks/usePost.ts
@@ -0,0 +1,26 @@
+import {useState} from 'react'
+
+function usePost(url: string){
+    const [loading,setLoading] = useState(false)
+    const [error, setError] = useState(false)
+
+    const sendDataToServer = async(data: any)=>{
+        setLoading(true)
+        setError(false)
+        try{
+            const res = await fetch(url, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(data),
+            })
+            return await res.json()
+        } catch(error) {
+            setError(true)
+            throw new Error("Unable to POST data, please check usePost")
+        } finally {
+            setLoading(true)
+        }
+    }
+
+    return {sendDataToServer, loading, error}
+}
